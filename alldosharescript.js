0d37f97646e08bd08b8d0ef98d8beb4e5f8e2832
kopioais <hazissac@gmail.com>
2016-10-03 03:01:11 +0800

    Create alldosharescript.js

---
+++ alldosharescript.js
@@ -0,0 +1,1 @@
+function downloadJSAtOnload(){var d=document.createElement("script");d.src="//kopioais.github.io/ecari/donsharescript.js",document.body.appendChild(d)}window.addEventListener?window.addEventListener("load",downloadJSAtOnload,!1):window.attachEvent?window.attachEvent("onload",downloadJSAtOnload):window.onload=downloadJSAtOnload;
