4608c6d29341bcc9010e448df75db177018ff2bc
kopioais <hazissac@gmail.com>
2016-10-03 03:12:41 +0800

    Update alldosharescript.js

--- alldosharescript.js
+++ alldosharescript.js
@@ -1,3 +1,5 @@
+<script type='text/javascript'>
 //<![CDATA[
 function downloadJSAtOnload(){var d=document.createElement("script");d.src="//kopioais.github.io/ecari/donsharescript.js",document.body.appendChild(d)}window.addEventListener?window.addEventListener("load",downloadJSAtOnload,!1):window.attachEvent?window.attachEvent("onload",downloadJSAtOnload):window.onload=downloadJSAtOnload;
 //]]>
+</script>
